26701df851bbae10d2555fbcee8ceabe11ccdbb3
Dinu Neculai <nicu.dinu.neculai@gmail.com>
2024-05-20 21:11:28 +0300

    Initial Commit

---
+++ src/main/java/dinu/imeserias/IMeseriasApplication.java
@@ -0,0 +1,13 @@
+package dinu.imeserias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMeseriasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IMeseriasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dinu/imeserias/IMeseriasApplicationTests.java
@@ -0,0 +1,13 @@
+package dinu.imeserias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IMeseriasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
